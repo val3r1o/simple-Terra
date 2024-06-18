59e25ef09c28e7076cb4ef710f5be09950211ade
Valerio Martini <valerio.martini@gmail.com>
2024-06-18 23:23:28 +0200

    Update retrive_id.tf

--- terra01/retrive_id.tf
+++ terra01/retrive_id.tf
@@ -8,3 +8,11 @@ data "apstra_datacenter_systems" "pod1" {
 output "pod1" {
   value = data.apstra_datacenter_systems.pod1.ids
 }
+
+# --
+
+data "apstra_datacenter_virtual_network_binding_constructor" "dc1" {
+  blueprint_id = "c76d3c1f-bfc8-4aae-b27b-d2ec6369331e"
+  # vlan_id      = 5 // optional; default behavior allows Apstra to choose
+  switch_ids   = ["leaf1", "leaf2", "leaf3"]
+}
