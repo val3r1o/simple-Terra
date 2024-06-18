5a2bc67fd57f6579591e33072cc521dda68329bf
Valerio Martini <valerio.martini@gmail.com>
2024-06-18 23:00:46 +0200

    Create provider.tf

---
+++ terra5/provider.tf
@@ -0,0 +1,19 @@
+# Terraform provder apstra
+terraform {
+        required_providers {
+                apstra = {
+                        source = "Juniper/apstra"
+                        version = "0.58.2"
+                }
+        }
+}
+
+# Apstra Provider Details
+provider "apstra" {
+  url                     = "https://admin:pass@172.20.114.3"
+  tls_validation_disabled = true                         # optional
+  blueprint_mutex_enabled = false
+  experimental            = true
+  api_timeout             = 0                            # optional; 0 == infinite
+
+}
