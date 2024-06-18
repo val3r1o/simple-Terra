b6395b8761237d391e5de5bde7e494ff2072fbb8
Valerio Martini <valerio.martini@gmail.com>
2024-06-18 23:15:16 +0200

    Update VRF.tf

--- terra11/VRF.tf
+++ terra11/VRF.tf
@@ -1,10 +1,12 @@
+
 # This example creates a routing zone within an
 # existing datacenter blueprint.
-resource "c76d3c1f-bfc8-4aae-b27b-d2ec6369331e" "new-vrf" {
-  name         = "new-vrf"
+
+resource "apstra_datacenter_routing_zone" "blue" {
+  name         = "vrf blue"
   blueprint_id = "c76d3c1f-bfc8-4aae-b27b-d2ec6369331e"
-  vlan_id      = 50                                    # optional
-  vni          = 500050                                 # optional
-  # dhcp_servers = ["192.168.100.10", "192.168.200.10"] # optional
+  vlan_id      = 5                                    # optional
+  vni          = 5000                                 # optional
+  dhcp_servers = ["192.168.100.10", "192.168.200.10"] # optional
   #  routing_policy_id = "<routing-policy-node-id-goes-here>" # optional
 }
