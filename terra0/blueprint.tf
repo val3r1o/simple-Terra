bd4565f40a39b23ed523f43a64d6d0f1446f3ca1
Valerio Martini <valerio.martini@gmail.com>
2024-06-18 23:41:02 +0200

    Update blueprint.tf

--- terra0/blueprint.tf
+++ terra0/blueprint.tf
@@ -1,4 +1,17 @@
 
+
+
+
+
+# This example outputs a list of blueprint IDs
+data "apstra_blueprints" "dc1" {
+  reference_design = "datacenter" // optional filter argument
+}
+
+output "apstra_blueprints" {
+  value = data.apstra_blueprints.dc1
+}
+
 output "print_bluePrint_id" {
         value = apstra_datacenter_blueprint.dc1.id
 }
