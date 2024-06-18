23bb29a71afc121adfed64864187696695fef31f
Valerio Martini <valerio.martini@gmail.com>
2024-06-19 00:36:14 +0200

    Update binding.tf

--- terra02/binding.tf
+++ terra02/binding.tf
@@ -2,5 +2,5 @@
 data "apstra_datacenter_virtual_network_binding_constructor" "dc1" {
   blueprint_id = "c76d3c1f-bfc8-4aae-b27b-d2ec6369331e"
   vlan_id      = 5 // optional; default behavior allows Apstra to choose
-  switch_ids   = [ "leaf1", leaf2]
+  switch_ids   = [ "leaf1", "leaf2"]
 }
