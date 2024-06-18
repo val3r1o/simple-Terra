f34a8cdace3b9cb3803a0f5950221fb9120b217d
Valerio Martini <valerio.martini@gmail.com>
2024-06-18 22:53:57 +0200

    Update and rename provider.tf to template.tf

--- terra2/template.tf
+++ terra2/template.tf
@@ -1,23 +1,3 @@
-# Terraform export for vJunOS with apstra
-terraform {
-        required_providers {
-                apstra = {
-                        source = "Juniper/apstra"
-                        version = "0.58.2"
-                }
-        }
-}
-
-# Apstra Provider Details
-provider "apstra" {
-  url                     = "https://admin:VictoriousJackal2%5E@172.20.114.3"
-  tls_validation_disabled = true                         # optional
-  blueprint_mutex_enabled = false
-  experimental            = true
-  api_timeout             = 0                            # optional; 0 == infinite
-
-}
-
 # Creating apstra design
 resource "apstra_logical_device" "leaf_1_QFX5120-48Y" {
         name = "leaf_1_QFX5120-48Y"
