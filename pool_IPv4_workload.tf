1335e885d26a22a9a55942011d698bda16c060db
Valerio Martini <valerio.martini@gmail.com>
2024-06-18 14:40:01 +0200

    Update pool_IPv4_workload.tf

--- pool_IPv4_workload.tf
+++ pool_IPv4_workload.tf
@@ -1,7 +1,6 @@
-
-## Create an IPv4 resource pool according to the instructions in the lab guide.
-resource "apstra_ipv4_pool" "lab_guide" {
-  name = "apstra-pool"
+## Create an IPv4 resource pool 
+resource "apstra_ipv4_pool" "lab_test" {
+  name = "pool_IPv4_workload"
   subnets = [
     { network = "192.168.100.0/24" },
     { network = "192.168.101.0/24" },
