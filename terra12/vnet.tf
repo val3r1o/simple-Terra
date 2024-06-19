78eec11249bcf24d068018f8e2a088370c6cffbd
Valerio Martini <valerio.martini@gmail.com>
2024-06-19 09:55:39 +0200

    Update vnet.tf

--- terra12/vnet.tf
+++ terra12/vnet.tf
@@ -3,16 +3,15 @@ data "apstra_datacenter_blueprint" "terra" {
   name = "dc1"
 }
 
-  #ROUTING-ZONE
-  resource "apstra_datacenter_routing_zone" "blue" {
-    name         = "blue"
-    blueprint_id = apstra_datacenter_blueprint.terra.id
-    vni          = 60000
-  
-  }
+
+data "apstra_datacenter_routing_zones" "blue" {
+  blueprint_id = data.apstra_datacenter_blueprint.terra.id
+
+}
+
 
 data "apstra_datacenter_systems" "leafs" {
-    blueprint_id = apstra_datacenter_blueprint.terra.id
+    blueprint_id = data.apstra_datacenter_blueprint.terra.id
     filters = [
       {
       role        = "leaf"
@@ -20,17 +19,17 @@ data "apstra_datacenter_systems" "leafs" {
     }
    ]
   }
-  
+
   data "apstra_datacenter_virtual_network_binding_constructor" "vnet_bindng_constructor" {
-    blueprint_id = apstra_datacenter_blueprint.terra.id
+    blueprint_id = data.apstra_datacenter_blueprint.terra.id
     switch_ids   = data.apstra_datacenter_systems.leafs.ids
   }
-  
+
   resource "apstra_datacenter_virtual_network" "app_networks" {
     name                         = "test-vn"
-    blueprint_id                 = apstra_datacenter_blueprint.terra.id
+    blueprint_id                 = data.apstra_datacenter_blueprint.terra.id
     type                         = "vxlan"
-    routing_zone_id              = apstra_datacenter_routing_zone.blue.id
+    routing_zone_id              = data.apstra_datacenter_routing_zone.blue.id
     ipv4_connectivity_enabled    = true
     ipv4_virtual_gateway_enabled = true
     bindings                     = data.apstra_datacenter_virtual_network_binding_constructor.vnet_bindng_constructor.bindings
