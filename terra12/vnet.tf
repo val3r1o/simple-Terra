3b79a5252fa4a9e6c165b27607f31e789a371cf5
Valerio Martini <valerio.martini@gmail.com>
2024-06-19 10:02:35 +0200

    Update vnet.tf

--- terra12/vnet.tf
+++ terra12/vnet.tf
@@ -3,14 +3,12 @@ data "apstra_datacenter_blueprint" "terra" {
   name = "dc1"
 }
 
-
   resource "apstra_datacenter_routing_zone" "blue" {
     name         = "blue"
-    blueprint_id = apstra_datacenter_blueprint.terra.id
+    blueprint_id = data.apstra_datacenter_blueprint.terra.id
     vni          = 500010
-  
-  }
 
+  }
 
 data "apstra_datacenter_systems" "leafs" {
     blueprint_id = data.apstra_datacenter_blueprint.terra.id
@@ -28,10 +26,10 @@ data "apstra_datacenter_systems" "leafs" {
   }
 
   resource "apstra_datacenter_virtual_network" "app_networks" {
-    name                         = "test-vn"
+    name                         = "terraa-vni"
     blueprint_id                 = data.apstra_datacenter_blueprint.terra.id
     type                         = "vxlan"
-    routing_zone_id              = data.apstra_datacenter_routing_zone.blue.id
+    routing_zone_id              = apstra_datacenter_routing_zone.blue.id
     ipv4_connectivity_enabled    = true
     ipv4_virtual_gateway_enabled = true
     bindings                     = data.apstra_datacenter_virtual_network_binding_constructor.vnet_bindng_constructor.bindings
