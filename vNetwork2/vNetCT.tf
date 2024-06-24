27848d142f9b4a4898668875328da9740a77dc24
Valerio Martini <valerio.martini@gmail.com>
2024-06-24 21:59:01 +0200

    Update vNetCT.tf

--- vNetwork2/vNetCT.tf
+++ vNetwork2/vNetCT.tf
@@ -66,8 +66,26 @@ resource "apstra_datacenter_connectivity_template" "CTVLAN100" {
   ]
 }
 
- data "apstra_datacenter_generic_system" "GEN-SYS" {
+ resource "apstra_datacenter_generic_system" "GEN-SYS" {
   blueprint_id      = data.apstra_datacenter_blueprint.POD1.id
   name              = "Sys101"
   hostname          = "Sys101"
- }
+  links = [
+    {
+      tags                          = ["SERVER"]
+      lag_mode                      = "lacp_active"
+      target_switch_id              = tolist(data.apstra_datacenter_systems.LEAVES.ids) [0] // first switch
+      target_switch_if_name         = "et-0/0/10"
+      target_switch_if_transform_id = 1
+      group_label                   = "bond0"
+    },
+    {
+      tags                          = ["SERVER"]
+      lag_mode                      = "lacp_active"
+      target_switch_id              = tolist(data.apstra_datacenter_systems.LEAVES.ids) [1] // second switch
+      target_switch_if_name         = "et-0/0/10"
+      target_switch_if_transform_id = 1
+      group_label                   = "bond0"
+    },
+  ]
+}
