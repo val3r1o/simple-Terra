091090e8fb0e3ba6a21f8a5c8e3312eae335e79b
Valerio Martini <valerio.martini@gmail.com>
2024-07-01 15:39:58 +0200

    Update vnet.tf

--- vNetwork/vnet.tf
+++ vNetwork/vnet.tf
@@ -1,17 +1,15 @@
-
-data "apstra_datacenter_blueprint" "terra" {
+data "apstra_datacenter_blueprint" "POD1" {
   name = "Cisco-DC1"
 }
 
-  resource "apstra_datacenter_routing_zone" "blue" {
-    name         = "blue-terra"
-    blueprint_id = data.apstra_datacenter_blueprint.terra.id
-    vni          = 500010
+resource "apstra_datacenter_routing_zone" "BLUE-VRF" {
+  name         = "blue-terra"
+  blueprint_id = data.apstra_datacenter_blueprint.POD1.id
+  vni          = 500010
+}
 
-  }
-
-data "apstra_datacenter_systems" "leafs" {
-    blueprint_id = data.apstra_datacenter_blueprint.terra.id
+data "apstra_datacenter_systems" "LEAVES" {
+    blueprint_id = data.apstra_datacenter_blueprint.POD1.id
     filters = [
       {
       role        = "leaf"
@@ -20,19 +18,20 @@ data "apstra_datacenter_systems" "leafs" {
    ]
   }
 
-  data "apstra_datacenter_virtual_network_binding_constructor" "vnet_bindng_constructor" {
-    blueprint_id = data.apstra_datacenter_blueprint.terra.id
-    switch_ids   = data.apstra_datacenter_systems.leafs.ids
-  }
+data "apstra_datacenter_virtual_network_binding_constructor" "VNETBIND" {
+  blueprint_id = data.apstra_datacenter_blueprint.POD1.id
+  vlan_id      = 100 
+  switch_ids   = data.apstra_datacenter_systems.LEAVES.ids
+}
 
-  resource "apstra_datacenter_virtual_network" "app_networks" {
-    name                         = "terra-vni"
-    blueprint_id                 = data.apstra_datacenter_blueprint.terra.id
-    type                         = "vxlan"
-    routing_zone_id              = apstra_datacenter_routing_zone.blue.id
-    ipv4_connectivity_enabled    = true
-    ipv4_virtual_gateway_enabled = true
-    reserve_vlan                 = "yes"
-    vni                          = 10000010
-    bindings                     = data.apstra_datacenter_virtual_network_binding_constructor.vnet_bindng_constructor.bindings
-  }
+resource "apstra_datacenter_virtual_network" "VLAN100" {
+  name                         = "vxlan-100-blue"
+  blueprint_id                 = data.apstra_datacenter_blueprint.POD1.id
+  type                         = "vxlan"
+  routing_zone_id              = apstra_datacenter_routing_zone.BLUE-VRF.id
+  ipv4_connectivity_enabled    = true
+  ipv4_virtual_gateway_enabled = true
+  ipv4_virtual_gateway         = "10.0.100.1"
+  ipv4_subnet                  = "10.0.100.0/24"
+  bindings = data.apstra_datacenter_virtual_network_binding_constructor.VNETBIND.bindings
+}
