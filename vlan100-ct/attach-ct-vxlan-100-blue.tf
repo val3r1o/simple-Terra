a42ca574b6a3b41d371061a0f51c463953e8a656
Valerio Martini <valerio.martini@gmail.com>
2024-07-01 21:43:57 +0200

    Update attach-ct-vxlan-100-blue.tf

--- vlan100-ct/attach-ct-vxlan-100-blue.tf
+++ vlan100-ct/attach-ct-vxlan-100-blue.tf
@@ -21,3 +21,12 @@ resource "apstra_datacenter_connectivity_template" "CTVLAN100" {
     data.apstra_datacenter_ct_virtual_network_single.TAGVLAN100.primitive
   ]
 }
+
+resource "apstra_datacenter_connectivity_template_assignment" "ASSIGN" {
+  blueprint_id               = data.apstra_datacenter_blueprint.POD1.id
+  application_point_id       = "FkYtMBdeoJ5urBaIEi8"
+  connectivity_template_ids   = [
+    "bcbcb35f-8f23-4bfb-916e-1b21d07d6904",
+    "1f8ac61f-6996-42bb-a34f-4f4a50a7111a",
+  ]
+}
